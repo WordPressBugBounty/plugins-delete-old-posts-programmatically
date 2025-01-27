b4d287c6425fb430852fc4e96282dbe9e8b48562
Sean Murphy <sean@iamseanmurphy.com>
2025-01-27 21:17:23 +0000

    3.9.7

--- delete-old-posts-programmatically/delete-old-posts.php
+++ delete-old-posts-programmatically/delete-old-posts.php
@@ -7,7 +7,7 @@
  * Author URI:  https://wpmagic.cloud
  * License:     GPL3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Version:     3.9.6
+ * Version:     3.9.7
  * Text Domain: delete-old-posts
  *
  * @package headless-cms
